cbb2ee5c6701cdec57e329c362adb6220a1bc9ec
snedvesky <thenuggetry@Scotts-MacBook-Pro.local>
2017-02-27 18:38:04 -0700

    Update HW2
    Finally got apigee to work

--- hw2.js
+++ hw2.js
@@ -5,76 +5,68 @@
 var express = require('express');
 var app = express();
 
-// GETs
+usergrid.init({
+    orgId: orgName,
+    appId: appName,
+    baseUrl: ""https://apibaas-trial.apigee.net""
+});
+
+// GET PROCEDURE
 app.get('/gets', function(req, res){
     if (req.method === 'GET')
-        if (Object.keys(req.query).length > 0) {
+        if (Object.keys(req.query) !== null) {
             res.send('GET:' + req.query);
         } else {
             res.send('Neither a header or query parameter were successfully sent!');
     }
 });
 
-// POST
+// POST PROCEDURE
 app.post('/posts', function (req, res) {
     if (req.method === 'POST')
-        if (Object.keys(req.query).length > 0) {
+        if (Object.keys(req.query) !== null) {
             res.send('POST:' + req.query);
         } else {
             res.send('Neither a header or query parameter were successfully sent!');
     }
 });
 
-// PUT
+// PUT PROCEDURE
 app.put('/puts', function (req, res) {
     if (req.method === 'PUT')
-        if (Object.keys(req.query).length > 0) {
+        if (Object.keys(req.query) !== null) {
             res.send('PUT:' + req.query);
         } else {
             res.send('Neither a header or query parameter were successfully sent!');
     }
 });
 
-// DELETE
+// DELETE PROCEDURE
 app.delete('/deletes', function (req, res) {
     if (req.method === 'DELETE')
-        if (Object.keys(req.query).length > 0) {
+        if (Object.keys(req.query) !== null) {
             res.send('DELETE:' + req.query);
     } else {
         res.send('Neither a header or query parameter were successfully sent!');
     }
 });
 
-
-// Bad Request Response
-app.use('*', function(req, res, next) {
-    var ER = new Error();
-    ER.status = 404;
-    ER.message = "Rejected: HTTP method Not Accepted";
-    next(ER);
-});
-
 //  Base URL Request Rejection
 app.all('/', function (req, res) {
-
     res.status(403).send('Rejected: URN Not Specified');
 });
 
 //  404 Errors
 app.use(function(err, req, res, next) {
-
     if(err.status !== 404) {
         return next();
     }
-
     res.send(err.message || ' nada ');
 });
 
 app.listen(8080, function() {
-
     var p1 = server.address().port
     var h1 = server.address().address
-
     console.log("Server running on http://%s:%s", h1, p1)
 
 }
